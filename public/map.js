097f07270e9b2a13deb6822baaa6eae6c8fcb2e9
FluffigKod <potatis@heiman.se>
2020-06-26 11:13:54 +0200

    add intro screen and refactor code into oop

--- public/map.js
+++ public/map.js
@@ -39,35 +39,160 @@ const redIcon = new L.Icon({
 });
 
 ///////////////////////////
-// SELECTORS
+// GAME CONTROLLER
 ///////////////////////////
-const sweatpointsLabel = document.querySelector('#sweatpoints');
+class GameController {
+  constructor() {
+    this.sweatpoints = 0;
+    this.questions = []
+    this.activeQuestion = null;
+    this.coordinates = this.getCoordinates();
+
+    fetch(`https://opentdb.com/api.php?amount=${this.coordinates.length}&type=multiple`)
+        .then((res) => res.json())
+        .then((data) => {
+            for (let [i, q] of data.results.entries()) {
+                let answers = q.incorrect_answers;
+                answers.push(q.correct_answer);
+                shuffleArray(answers);
+
+                let question = new Question(
+                    q.question,
+                    answers,
+                    q.correct_answer,
+                    this.coordinates[i]
+                );
+                this.questions.push(question);
+            }
+
+            this.getRandomQuestion();
+
+            this.addQuestionsToMap();
+
+            // showQuestion(activeQuestion);
+        });
+  }
+  onLocationFound(e) {
+      let radius = e.accuracy / proximity;
+
+      player.setLatLng(e.latlng);
+      playerCircle.setRadius(radius);
+      playerCircle.setLatLng(e.latlng);
+
+      if (this.activeQuestion) {
+          // Check if player is on question1
+          let distance = map.distance(
+              player.getLatLng(),
+              this.activeQuestion.marker.getLatLng()
+          );
+
+          if (distance < radius) {
+              playerCircle.setStyle({ fillColor: 'green' });
+              this.showQuestion(this.activeQuestion);
+          }
+      }
+  }
+  onLocationError(e) {
+      alert(e.message);
+  }
+  getCoordinates() {
+    let coordinates = [
+        [59.306222, 18.102063],
+        [59.304843, 18.102574],
+        [59.306877, 18.106906],
+        [59.305608, 18.099358],
+        [59.304148, 18.095912],
+        [59.304259, 18.110573],
+    ];
+    return coordinates;
+  }
+  addQuestionsToMap() {
+      for (let question of this.questions) {
+          question.addToMap();
+      }
+  }
+  giveSweatpoints(amount) {
+      this.sweatpoints += amount;
+      sweatpointsLabel.innerText = this.sweatpoints;
+  }
+  allQUestionsCompleted() {
+      for (let question of this.questions) {
+          if (question.completed == false) {
+              return false;
+          }
+      }
+      return true;
+  }
+  getRandomQuestion() {
+      if (this.allQUestionsCompleted()) {
+          let maxSweatpoints = 20 * this.questions.length;
+          Swal.fire(
+              'Alla avklarade!',
+              `Snyggt jobbat! Du samlade ${this.sweatpoints}/${maxSweatpoints} sweatpoints!!`,
+              'success'
+          );
+          map.stopLocate();
+      } else {
+          let question = this.questions[Math.floor(Math.random() * this.questions.length)];
+          if (question.completed) {
+              this.getRandomQuestion();
+          } else {
+              this.activeQuestion = question;
+              this.activeQuestion.activate();
+          }
+      }
+  }
+  showQuestion(question) {
+      if (!swal.isVisible() && !question.completed) {
+          Swal.fire({
+              title: question.question,
+              input: 'radio',
+              inputOptions: question.answers,
+              inputValidator: (value) => {
+                  if (!value) {
+                      return 'Du behöver välja ett alternativ!';
+                  }
+              },
+          }).then((result) => {
+              question.completed = true;
+
+              // remove marker
+              map.removeLayer(question.marker);
+              playerCircle.setStyle({ fillColor: 'blue' });
+              let playerChoice = question.answers[result.value];
+
+              if (playerChoice == question.correct) {
+                  // Correct
+                  this.giveSweatpoints(20);
+
+                  Swal.fire('Rätt!', 'Du fick 20 sweatpoints!', 'success').then(
+                      () => {
+                          // get a new question
+                          this.getRandomQuestion();
+                      }
+                  );
+              } else {
+                  // incorrect
+                  this.giveSweatpoints(10);
+
+                  Swal.fire(
+                      'Tyvärr var det fel!',
+                      `Rätt svar var "${question.correct}". Du fick ändå 10 sweatpoints för att du tog dig hit! Ta nästa fråga istället!`,
+                      'error'
+                  ).then(() => {
+                      // get a new question
+                      this.getRandomQuestion();
+                  });
+              }
+          });
+      }
+  }
+}
 
 ///////////////////////////
-// SETUP
+// MARKER CONTROLLER
 ///////////////////////////
-const proximity = 2;
-let sweatpoints = 0;
-let initQuestions = false;
 
-// Create new map
-var map = L.map('map', {
-    enableHighAccuracy: true,
-}).fitWorld();
-
-// Get graphics for map
-L.tileLayer(
-    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
-    {
-        attribution:
-            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 30,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: token,
-    }
-).addTo(map);
 
 ///////////////////////////
 // QUESTION
@@ -99,61 +224,6 @@ class Question {
     }
 }
 
-// All questions
-// new Question(
-//     '',
-//     ['', '', ''],
-//     2,
-//     [, ]
-// ),
-
-// let questions = [
-//     new Question(
-//         'Hur många sekunder tar det för solens ljus att nå jorden?',
-//         ['125s', '499s', '834.57s'],
-//         1,
-//         [59.306222, 18.102063]
-//     ),
-//     new Question(
-//         'Vem anses vara den första programmeraren?',
-//         ['Alan Turing', 'Bill Gates', 'Ada Lovelace'],
-//         2,
-//         [59.304843, 18.102574]
-//     ),
-//     new Question(
-//         'När skapades det första datorviruset?',
-//         ['1983', '2001', '1965'],
-//         0,
-//         [59.306877, 18.106906]
-//     ),
-//     new Question(
-//         'Vad hette det första programmeringsspråket?',
-//         ['Java', 'Fortran', 'Python'],
-//         1,
-//         [59.305608, 18.099358]
-//     ),
-//     new Question(
-//         'Vilken planet skulle flyta om man la den i vatten?',
-//         ['Merkurius', 'Neptunus', 'Saturnus'],
-//         2,
-//         [59.304148, 18.095912]
-//     ),
-//     new Question(
-//         'Hur snabbt kan neutronstjärnor rotera per sekund?',
-//         ['500', '0.5', '200'],
-//         0,
-//         [59.304259, 18.110573]
-//     ),
-// ];
-
-let coordinates = [
-    [59.306222, 18.102063],
-    [59.304843, 18.102574],
-    [59.306877, 18.106906],
-    [59.305608, 18.099358],
-    [59.304148, 18.095912],
-    [59.304259, 18.110573],
-];
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -162,168 +232,79 @@ function shuffleArray(array) {
     }
 }
 
-function giveSweatpoints(amount) {
-    sweatpoints += amount;
-    sweatpointsLabel.innerText = sweatpoints;
-}
-
-function addQuestionsToMap() {
-    for (let question of questions) {
-        question.addToMap();
-    }
-}
-
-function allQUestionsCompleted() {
-    for (let question of questions) {
-        if (question.completed == false) {
-            return false;
-        }
-    }
-    return true;
-}
-
-// Get random question
-function getRandomQuestion() {
-    if (allQUestionsCompleted()) {
-        let maxSweatpoints = 20 * questions.length;
-        Swal.fire(
-            'Alla avklarade!',
-            `Snyggt jobbat! Du samlade ${sweatpoints}/${maxSweatpoints} sweatpoints!!`,
-            'success'
-        );
-        map.stopLocate();
-    } else {
-        let question = questions[Math.floor(Math.random() * questions.length)];
-        if (question.completed) {
-            getRandomQuestion();
-        } else {
-            activeQuestion = question;
-            activeQuestion.activate();
-        }
-    }
-}
-
-function showQuestion(question) {
-    if (!swal.isVisible() && !question.completed) {
-        Swal.fire({
-            title: question.question,
-            input: 'radio',
-            inputOptions: question.answers,
-            inputValidator: (value) => {
-                if (!value) {
-                    return 'Du behöver välja ett alternativ!';
-                }
-            },
-        }).then((result) => {
-            question.completed = true;
-
-            // remove marker
-            map.removeLayer(question.marker);
-            playerCircle.setStyle({ fillColor: 'blue' });
-            let playerChoice = question.answers[result.value];
-
-            if (playerChoice == question.correct) {
-                // Correct
-                giveSweatpoints(20);
-
-                Swal.fire('Rätt!', 'Du fick 20 sweatpoints!', 'success').then(
-                    () => {
-                        // get a new question
-                        getRandomQuestion();
-                    }
-                );
-            } else {
-                // incorrect
-                giveSweatpoints(10);
-
-                Swal.fire(
-                    'Tyvärr var det fel!',
-                    `Rätt svar var "${question.correct}". Du fick ändå 10 sweatpoints för att du tog dig hit! Ta nästa fråga istället!`,
-                    'error'
-                ).then(() => {
-                    // get a new question
-                    getRandomQuestion();
-                });
-            }
-        });
-    }
-}
-
-// Get random question and activate
-let questions = [];
-let activeQuestion = null;
-
-fetch('https://opentdb.com/api.php?amount=6&type=multiple')
-    .then((res) => res.json())
-    .then((data) => {
-        for (let [i, q] of data.results.entries()) {
-            let answers = q.incorrect_answers;
-            answers.push(q.correct_answer);
-            shuffleArray(answers);
-
-            let question = new Question(
-                q.question,
-                answers,
-                q.correct_answer,
-                coordinates[i]
-            );
-            questions.push(question);
-        }
-
-        getRandomQuestion();
-
-        addQuestionsToMap();
-
-        // showQuestion(activeQuestion);
-    });
-
-function onLocationFound(e) {
-    let radius = e.accuracy / proximity;
-
-    player.setLatLng(e.latlng);
-    playerCircle.setRadius(radius);
-    playerCircle.setLatLng(e.latlng);
-
-    if (activeQuestion) {
-        // Check if player is on question1
-        let distance = map.distance(
-            player.getLatLng(),
-            activeQuestion.marker.getLatLng()
-        );
-
-        if (distance < radius) {
-            playerCircle.setStyle({ fillColor: 'green' });
-            showQuestion(activeQuestion);
-        }
-    }
-}
-
-function onLocationError(e) {
-    alert(e.message);
-}
-
 function changeLocateMaxZoom(e) {
     if (map._locateOptions) {
         map._locateOptions.maxZoom = map.getZoom();
     }
 }
 
+function fade(callback) {
+  $(overlay).animate({
+    opacity: 1
+  }, 2000, function() {
+    callback()
+
+    $(overlay).animate({
+      opacity: 0
+    }, 2000)
+  })
+}
+
+///////////////////////////
+// SELECTORS
+///////////////////////////
+const sweatpointsLabel = document.querySelector('#sweatpoints');
+const playBtn = document.querySelector('#play-btn');
+const markerBtn = document.querySelector('#marker-btn');
+const overlay = document.querySelector('#overlay');
+const introScreen = document.querySelector('#intro-screen');
+
+///////////////////////////
+// SETUP
+///////////////////////////
+const proximity = 2;
+
+// Create new map
+var map = L.map('map', {
+    enableHighAccuracy: true,
+}).fitWorld();
+
+// Get graphics for map
+L.tileLayer(
+    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+    {
+        attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 30,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: token,
+    }
+).addTo(map);
+
 // Player marker
 let player = L.marker(map.getCenter(), { icon: redIcon }).addTo(map);
 let playerCircle = L.circle(map.getCenter(), 0).addTo(map);
 
-map.locate({
-    setView: true,
-    maxZoom: 18,
-    watch: true,
-    enableHighAccuracy: true,
-});
-
 ///////////////////////////
 // EVENT LISTENERS
 ///////////////////////////
-map.on('locationfound', onLocationFound);
-map.on('locationerror', onLocationError);
 map.on('zoomend', changeLocateMaxZoom);
 
-// window.navigator.vibrate(200);
+playBtn.addEventListener('click', function() {
+  fade(function() {
+    game = new GameController();
+    map.on('locationfound', game.onLocationFound);
+    map.on('locationerror', game.onLocationError);
+
+    introScreen.style.display = 'none';
+
+    map.locate({
+        setView: true,
+        maxZoom: 18,
+        watch: 'true',
+        enableHighAccuracy: true,
+    });
+  });
+})
---
+++ public/sampleQuestions.js
@@ -0,0 +1,46 @@
+// All questions
+// new Question(
+//     '',
+//     ['', '', ''],
+//     2,
+//     [, ]
+// ),
+
+let questions = [
+    new Question(
+        'Hur många sekunder tar det för solens ljus att nå jorden?',
+        ['125s', '499s', '834.57s'],
+        1,
+        [59.306222, 18.102063]
+    ),
+    new Question(
+        'Vem anses vara den första programmeraren?',
+        ['Alan Turing', 'Bill Gates', 'Ada Lovelace'],
+        2,
+        [59.304843, 18.102574]
+    ),
+    new Question(
+        'När skapades det första datorviruset?',
+        ['1983', '2001', '1965'],
+        0,
+        [59.306877, 18.106906]
+    ),
+    new Question(
+        'Vad hette det första programmeringsspråket?',
+        ['Java', 'Fortran', 'Python'],
+        1,
+        [59.305608, 18.099358]
+    ),
+    new Question(
+        'Vilken planet skulle flyta om man la den i vatten?',
+        ['Merkurius', 'Neptunus', 'Saturnus'],
+        2,
+        [59.304148, 18.095912]
+    ),
+    new Question(
+        'Hur snabbt kan neutronstjärnor rotera per sekund?',
+        ['500', '0.5', '200'],
+        0,
+        [59.304259, 18.110573]
+    ),
+];
